b71abec497a8a96e67258a30bf68703db94f0bbf
ApplicationUser <dimkabelkin1@gmail.com>
2018-02-09 21:30:31 +0300

    Исправлен баг

--- unsubscribe.js
+++ unsubscribe.js
@@ -3,7 +3,7 @@ var unsubscribe_button = document.getElementsByClassName("flat_button button_sma
 var count = unsubscribe_button.length,
 	i = 0;
 function unsubscribe_all() {
-	unsubscribe_button[i++].click();
+	unsubscribe_button[0].click();
 }
 
 setInterval(unsubscribe_all, 1500);
